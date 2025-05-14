6b3ce91fe3eecbf7373caded3f95ee93a17dc1c2
Vo Thi Hong Ha <93961270+Hahaha2808@users.noreply.github.com>
2025-05-14 21:57:24 +0700

    Add tenant API

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
 import roomRouter from "./routes/roomRoutes.js";
+import tenantRoutes from "./routes/tenantRoutes.js";
 dotenv.config();
 connectDB();
 
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/rooms", roomRouter);
+app.use("/api/tenant", tenantRoutes);
 
 app.use("/uploads", express.static("uploads"));
 const PORT = process.env.PORT || 5000;
