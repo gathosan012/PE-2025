b13fe155793a94ec2a8e40dbaa23f31df38980bf
gathosan012 <calanaustin77@gmail.com>
2025-06-12 11:24:18 +0700

    update url 02

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ connectDB();
 const app = express();
 app.use(
   cors({
-    origin: "https://pe-2025.onrender.com/",
+    origin: "https://app-frontend-tmwi.onrender.com",
     credentials: true,
   })
 );
